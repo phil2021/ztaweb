c43f905486c7d9bf65c959d66e2acd4760f43ec9
phil2021 <86551492+phil2021@users.noreply.github.com>
2022-12-02 19:25:18 +0800

    added auth middleware to get reviewing user details

--- src/routes/v1/review.route.js
+++ src/routes/v1/review.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { reviewController } = require('../../controllers');
+const auth = require('../../middlewares/auth');
 
 const router = express.Router({ mergeParams: true });
 
 const { setAttractionIds, createReview, getReviews, getReview, getReviewBySlug, updateReview, deleteReview } =
   reviewController;
 
-router.route('/').post(setAttractionIds, createReview).get(getReviews);
+router.route('/').post(auth(), setAttractionIds, createReview).get(getReviews);
 
 router.route('/:reviewId').get(getReview).patch(updateReview).delete(deleteReview);
 
