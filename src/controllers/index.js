4524c52db1d4bca98ed4187a305bd3fdd73cdeae
phil2021 <86551492+phil2021@users.noreply.github.com>
2022-12-02 19:26:05 +0800

    Added activity controller to the index file

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,3 +3,4 @@ module.exports.userController = require('./user.controller');
 module.exports.destinationController = require('./destination.controller');
 module.exports.attractionController = require('./attraction.controller');
 module.exports.reviewController = require('./review.controller');
+module.exports.activityController = require('./activity.controller');
