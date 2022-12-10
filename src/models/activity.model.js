c0ef8bcedbef14f0ae33952f4c858ac97f1c7cf1
phil2021 <86551492+phil2021@users.noreply.github.com>
2022-12-10 08:07:02 +0800

    modified the model to contain more info

--- src/models/activity.model.js
+++ src/models/activity.model.js
@@ -1,19 +1,93 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
+const { Schema } = mongoose;
 const { toJSON, paginate } = require('./plugins');
 
-const activitySchema = mongoose.schema(
+const activitySchema = Schema(
   {
-    name: String,
-    description: String,
+    name: {
+      type: String,
+      required: [true, 'An activity must have a name'],
+      unique: true,
+      trim: true,
+      maxlength: [40, 'An activity name must be less than or equal to 40 characters'],
+      minlength: [10, 'An activity name must not be less than 10 characters'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'An activity must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator(val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price',
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'An activity must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'An activity must have a cover image'],
+    },
     images: [String],
-    booking: String,
+    duration: {
+      type: Number,
+      required: [true, 'An activity must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'An activity must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'An activity must have a difficulty.'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: '{VALUE} is not supported! Difficulty is either: easy, medium, or hard',
+      },
+    },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
     attraction: {
       type: mongoose.Types.ObjectId,
       ref: 'Attraction',
       required: [true, 'Activity must belong to an Attraction'],
     },
+    booking: String,
     slug: String,
   },
   {
@@ -28,10 +102,21 @@ activitySchema.plugin(toJSON);
 activitySchema.plugin(paginate);
 
 activitySchema.index({ attraction: 1, name: 1 }, { unique: true });
+activitySchema.index({ price: 1 });
+activitySchema.index({ slug: 1 });
+activitySchema.index({ startLocation: '2dsphere' });
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create() !.update()
 activitySchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// QUERY MIDDLEWARE:
+// findByIdAndUpdate
+// findByIdAndDelete
+activitySchema.pre(/^findByIdAnd/, async function (next) {
+  this.act = await this.findOne();
   next();
 });
 /**
