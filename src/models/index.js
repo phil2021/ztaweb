b32b554408d5c6aac8327fd9d8781438de74599f
phil2021 <86551492+phil2021@users.noreply.github.com>
2022-12-11 23:14:01 +0800

    added activity model

--- src/models/index.js
+++ src/models/index.js
@@ -4,3 +4,4 @@ module.exports.Operator = require('./operator.model');
 module.exports.Destination = require('./destination.model');
 module.exports.Attraction = require('./attraction.model');
 module.exports.Review = require('./review.model');
+module.exports.Activity = require('./activity.model');
